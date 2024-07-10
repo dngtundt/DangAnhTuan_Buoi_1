595aa2f9be612dbd1290fcaba1d4cf0554d3e6c5
dngtundt <dtun11111@gmail.com>
2024-07-10 15:40:26 +0700

    first commit

---
+++ DangAnhTuan_Buoi_1_KTLT/DangAnhTuan_Buoi_1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	float d;
+	printf("Nhap vao lan luot a, b, c: ");
+	scanf("%d%d%d", &a, &b, &c);
+	d = c / (a - b);
+	printf("Ket qua la: %f", d);
+}
