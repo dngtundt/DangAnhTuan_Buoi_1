4d40e12de5590d68e0004367037b490db6ec812e
dngtundt <dtun11111@gmail.com>
2024-07-17 11:58:43 +0700

    add 2 functions

--- DangAnhTuan_Buoi_1_KTLT/Bai_5_Buoi_1.cpp
+++ DangAnhTuan_Buoi_1_KTLT/Bai_5_Buoi_1.cpp
@@ -1,20 +1,20 @@
-﻿#include <stdio.h>
-#include <conio.h>
-
-int main()
-{
-	int a, b, c;
-	float result;
-	do
-	{
-		printf("Nhap vao gia tri cua a, b, c: ");
-		scanf("%d %d %d", &a, &b, &c);
-		if (a == b) {
-			printf("a va b phai khac nhau!\n");
-		}
-	} while (a == b);
-		result = (float)c / (a - b);
-		printf("Ket qua cua phep chia c/(a-b) la: %.2f\n", result);
-	return 0;
-	getch();
-}
+﻿//#include <stdio.h>
+//#include <conio.h>
+//
+//int main()
+//{
+//	int a, b, c;
+//	float result;
+//	do
+//	{
+//		printf("Nhap vao gia tri cua a, b, c: ");
+//		scanf("%d %d %d", &a, &b, &c);
+//		if (a == b) {
+//			printf("a va b phai khac nhau!\n");
+//		}
+//	} while (a == b);
+//		result = (float)c / (a - b);
+//		printf("Ket qua cua phep chia c/(a-b) la: %.2f\n", result);
+//	return 0;
+//	getch();
+//}
